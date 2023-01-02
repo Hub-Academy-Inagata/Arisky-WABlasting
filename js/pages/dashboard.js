d16df2b93960490549b3cfe7573c49df0ddc4dd1
Nabilla Zahra Trisnani <nabillatrisnani29@gmail.com>
2023-01-02 19:10:21 +0700

    fix: dashboard, buat pesan, login

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -10,8 +10,66 @@
 // random Numbers
 const random = () => Math.round(Math.random() * 200);
 
+//option untuk line chart
+const optionsLineChart = {
+  responsive: true, //untuk responsive
+  maintainAspectRatio: false, //untuk responsive
+  plugins: {
+    legend: {
+      display: false,
+    }
+  },
+  interaction: {
+    intersect: false,
+    mode: 'index',
+  },
+  scales: {
+    y: {
+      grid: {
+        drawBorder: false,
+        display: false,
+        drawOnChartArea: false,
+        drawTicks: false,
+      },
+      ticks: {
+        display: true,
+        padding: 10,
+        color: '#000000',
+        precision: 0,
+        font: {
+          size: 12,
+          family: "'Lato', sans-serif",
+          style: 'normal',
+          weight: 'bold',
+          lineHeight: 2
+        },
+      }
+    },
+    x: {
+      grid: {
+        drawBorder: false,
+        display: false,
+        drawOnChartArea: false,
+        drawTicks: false,
+      },
+      ticks: {
+        display: true,
+        color: '#000000',
+        padding: 8,
+        font: {
+          size: 14,
+          family: "'Lato', sans-serif",
+          style: 'normal',
+          weight: 'bold',
+          lineHeight: 2
+        },
+      }
+    },
+  },
+};
+
 // eslint-disable-next-line no-unused-vars
-const lineChart = new Chart(document.getElementById('canvas-1'), {
+const lineChartMingguan = new Chart(document.getElementById('canvas-mingguan'), {
   type: 'line',
   data: {
     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
@@ -19,28 +77,82 @@ const lineChart = new Chart(document.getElementById('canvas-1'), {
       label: 'Total Penerima',
       backgroundColor: 'rgba(228, 55, 49, 0.1)',
       borderColor: 'rgba(228, 55, 49, 1)',
-      pointBackgroundColor: 'rgba(228, 55, 49, 1)',
-      pointBorderColor: '#fff',
-      data: [random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random()]
+      pointBackgroundColor: '#FCEBEA',
+      pointBorderColor: '#E43631',
+      tension: 0,
+      pointRadius: 8,
+      pointBorderWidth: 2,
+      pointHoverRadius: 8,
+      pointHoverBorderWidth: 2,
+      pointHoverBackgroundColor: '#FCEBEA',
+      pointHoverBorderColor: '#E43631',
+      borderWidth: 2,
+      fill: true,
+      maxBarThickness: 6,
+      data: [50, 15, 30, 80, 60, 50, 15, 30, 80, 60, 175, 175]
     }]
   },
-  options: {
-    responsive: true
-  }
+  options: optionsLineChart
+});
+
+const lineChartBulanan = new Chart(document.getElementById('canvas-bulanan'), {
+  type: 'line',
+  data: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+    datasets: [{
+      label: 'Total Penerima',
+      backgroundColor: 'rgba(228, 55, 49, 0.1)',
+      borderColor: 'rgba(228, 55, 49, 1)',
+      pointBackgroundColor: '#FCEBEA',
+      pointBorderColor: '#E43631',
+      tension: 0,
+      pointRadius: 8,
+      pointBorderWidth: 2,
+      pointHoverRadius: 8,
+      pointHoverBorderWidth: 2,
+      pointHoverBackgroundColor: '#FCEBEA',
+      pointHoverBorderColor: '#E43631',
+      borderWidth: 2,
+      fill: true,
+      maxBarThickness: 6,
+      data: [50, 15, 30, 80, 60, 50, 15, 30, 80, 60, 175, 200]
+    }]
+  },
+  options: optionsLineChart
 });
 
 // eslint-disable-next-line no-unused-vars
-const doughnutChart = new Chart(document.getElementById('canvas-3'), {
+const doughnutChart = new Chart(document.getElementById('canvas-penerima'), {
     type: 'doughnut',
     data: {
       labels: ['Terkirim', 'Tidak Terkirim'],
       datasets: [{
         data: [150, 25],
         backgroundColor: ['#5CB85C', '#EC3832'],
-        hoverBackgroundColor: ['#5CB85C', '#EC3832']
+        hoverBackgroundColor: ['#5CB85C', '#EC3832'],
+        cutout: 60,
+        borderWidth: 0,
       }]
     },
     options: {
-      responsive: true
+      responsive: true, //untuk responsive
+      maintainAspectRatio: false, //untuk responsive
+      plugins: {
+        legend: {
+          position: "right",
+          labels: {
+            boxWidth: 12,
+            boxHeight: 12,
+            color: "#000000",
+            font: {
+              size: 12,
+              family: "'Lato', sans-serif",
+              style: 'normal',
+              weight: 'bold',
+              lineHeight: 16
+            },
+          }
+        }
+      },
     }
   });
